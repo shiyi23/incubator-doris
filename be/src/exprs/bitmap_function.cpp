361c80b10380510949d8d12e90b5914722da2ebb
ac23 <3045712342@qq.com>
2021-05-08 17:56:43 +0800

    更新bitmap_andnot函数相关

--- be/src/exprs/bitmap_function.cpp
+++ be/src/exprs/bitmap_function.cpp
@@ -530,6 +530,27 @@ StringVal BitmapFunctions::bitmap_not(FunctionContext* ctx, const StringVal& lhs
     return serialize(ctx, &bitmap);
 }
 
+//refer to : http://www.bricktou.com/lib/bitmap__bitmap_andnot_src.html
+StringVal BitmapFunctions::bitmap_andnot(FunctionContext* ctx, const StringVal& lhs,
+                                      const StringVal& rhs) {
+    if (lhs.is_null || rhs.is_null) {
+        return StringVal::null();
+    }
+    BitmapValue bitmap;
+    if (lhs.len = 0) {
+        bitmap |= *reinterpret_cast<BitmapValue*>(lhs.ptr);
+    } else {
+        bitmap |= BitmapValue((char*)lhs.ptr);
+    }
+    
+    if (rhs.len = 0) {
+        bitmap &= ~*reinterpret_cast<BitmapValue*>(rhs.ptr);
+    } else {
+        bitmap &= ~BitmapValue((char*)rhs.ptr);
+    }
+    return serialize(ctx, &bitmap);
+}
+
 StringVal BitmapFunctions::bitmap_to_string(FunctionContext* ctx, const StringVal& input) {
     if (input.is_null) {
         return StringVal::null();
